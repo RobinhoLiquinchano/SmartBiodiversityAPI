fc531a54d0d30b3a65b682252a3ed5354e9ed33c
User <User@DESKTOP-MCTRVFT>
2026-07-20 08:19:27 -0500

    FondEnd agregado

---
+++ SmartBiodiversityUtnMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartBiodiversityUtnMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
