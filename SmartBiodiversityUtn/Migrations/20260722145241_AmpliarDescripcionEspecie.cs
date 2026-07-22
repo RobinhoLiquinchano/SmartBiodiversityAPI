66242b98e19c9ccc185766bdecdfd17dbacd6705
User <User@DESKTOP-MCTRVFT>
2026-07-22 09:52:52 -0500

    nueva migracion

---
+++ SmartBiodiversityUtn/Migrations/20260722145241_AmpliarDescripcionEspecie.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBiodiversityUtn.Migrations
+{
+    /// <inheritdoc />
+    public partial class AmpliarDescripcionEspecie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DescripcionEsp",
+                table: "Especies",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DescripcionEsp",
+                table: "Especies",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
