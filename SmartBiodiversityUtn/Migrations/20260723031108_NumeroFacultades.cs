20e12983c889cc55c9ae39a282a971847272b690
User <User@DESKTOP-MCTRVFT>
2026-07-22 22:11:44 -0500

    Implementado numoer de facultad

---
+++ SmartBiodiversityUtn/Migrations/20260723031108_NumeroFacultades.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBiodiversityUtn.Migrations
+{
+    /// <inheritdoc />
+    public partial class NumeroFacultades : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumeroFac",
+                table: "Facultades",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroFac",
+                table: "Facultades");
+        }
+    }
+}
